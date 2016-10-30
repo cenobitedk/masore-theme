f28397b51671d4773034ac41635dc7b0928dc798
Jeppe Hasseriis <j.hasseriis@gmail.com>
2016-10-30 20:45:21 +0100

    Add style fonts

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function masore_theme_enqueue_styles() {
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/style.css', [], wp_get_theme()->get('Version'));
+  wp_enqueue_style('masore-fonts', get_stylesheet_directory_uri() . '/assets/fonts.css', [], wp_get_theme()->get('Version'));
+  wp_enqueue_style('masore-style', get_stylesheet_directory_uri() . '/assets/style.css', [], wp_get_theme()->get('Version'));
 }
 
 add_action('wp_enqueue_scripts', 'masore_theme_enqueue_styles');
