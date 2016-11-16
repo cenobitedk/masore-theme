5f84ea4b962247f83597221b453cf5e42788c40a
Jeppe Hasseriis <j.hasseriis@gmail.com>
2016-11-16 23:23:56 +0100

    tweak functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 function masore_theme_enqueue_styles() {
-  wp_enqueue_style('masore-fonts', get_stylesheet_directory_uri() . '/assets/fonts.css', [], wp_get_theme()->get('Version'));
-  wp_enqueue_style('masore-style', get_stylesheet_directory_uri() . '/assets/style.css', [], wp_get_theme()->get('Version'));
+  $theme = wp_get_theme('masore-theme');
+  wp_enqueue_style('masore-style', get_stylesheet_directory_uri() . '/assets/main.css', array(), $theme->get('Version'));
 }
 
 add_action('wp_enqueue_scripts', 'masore_theme_enqueue_styles');
