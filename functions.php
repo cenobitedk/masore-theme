9027b16bd5d132c48f94fec04e0d648994ee2521
Jeppe Hasseriis <j.hasseriis@gmail.com>
2016-11-17 21:44:32 +0100

    Add custom function to list all collections

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+function masore_get_collections() {
+	$collections = [];
+	$taxonomies = get_taxonomies();
+	if (in_array('collection', $taxonomies)) {
+		$collections = get_terms(array(
+			'taxonomy' => 'collection',
+			'parent'   => 0,
+			'hide_empty' => false,
+			'orderby' => 'term_id',
+		));
+	}
+	return $collections;
+}
+
 function masore_theme_enqueue_styles() {
   $theme = wp_get_theme('masore-theme');
   wp_enqueue_style('masore-style', get_stylesheet_directory_uri() . '/assets/main.css', array(), $theme->get('Version'));
