8d49f83c5defb4f22673d507afd7f1b10e9064cd
Jeppe Hasseriis <j.hasseriis@gmail.com>
2017-01-11 23:43:26 +0100

    Add custom filter for next and previous links.

--- functions.php
+++ functions.php
@@ -24,3 +24,15 @@ function masore_theme_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'masore_theme_enqueue_styles');
+
+function masore_adjacent_post_link($output, $format, $link, $post, $adjacent) {
+  if (empty($output)) {
+    $adjacent = $adjacent == 'next' ? 'previous' : 'next';
+    return '<span class="inactive">' . str_replace('%link', $adjacent, $format) . '</span>';
+  } else {
+    return $output;
+  }
+}
+
+add_filter('previous_post_link', 'masore_adjacent_post_link', 10, 5);
+add_filter('next_post_link', 'masore_adjacent_post_link', 10, 5);
