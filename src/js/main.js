f04618002938ad891352a0ba01c245cdbffea99b
Jeppe Hasseriis <j.hasseriis@gmail.com>
2017-01-25 20:27:35 +0100

    Javascript fix

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,4 @@ var LuminousGallery = require('luminous-lightbox').LuminousGallery;
 var images = document.querySelectorAll('.image-gallery');
 var gallery = new LuminousGallery(images);
 var fabric_images = document.querySelectorAll('.image-fabric');
-var fabric_gallery = new LuminousGallery(fabric_images, {
-  sourceAttribute: 'data-href'
-});
+var fabric_gallery = new LuminousGallery(fabric_images);
