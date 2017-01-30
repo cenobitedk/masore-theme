be8702725575ea4d6a2a257b27bb095184dba67e
Jeppe Hasseriis <j.hasseriis@gmail.com>
2017-01-30 23:56:50 +0100

    Fix collection overview sort order.

--- functions.php
+++ functions.php
@@ -18,6 +18,16 @@ function masore_get_post_collection($id) {
   return wp_get_object_terms($id, 'collection')[0];
 }
 
+function masore_filter_collection_query($query) {
+  // Modify query for displaying designs in a collection, sorted by custom field.
+  if(is_tax('collection') && !is_admin() && is_main_query()) {
+    $query->set('posts_per_page', '50');
+    $query->set('orderby', 'meta_value');
+    $query->set('meta_key', 'number');
+    $query->set('order', 'ASC');
+  }
+}
+
 function masore_theme_enqueue_assets() {
   $theme = wp_get_theme('masore-theme');
   $dir = get_stylesheet_directory_uri();
@@ -34,6 +44,7 @@ function masore_adjacent_post_link($output, $format, $link, $post, $adjacent) {
   }
 }
 
+add_action('pre_get_posts', 'masore_filter_collection_query');
 add_action('wp_enqueue_scripts', 'masore_theme_enqueue_assets');
 add_filter('previous_post_link', 'masore_adjacent_post_link', 10, 5);
 add_filter('next_post_link', 'masore_adjacent_post_link', 10, 5);
