8cb44889393ea0e7a984e7959bf5715ec0fbc5b6
Jeppe Hasseriis <j.hasseriis@gmail.com>
2017-02-03 23:57:45 +0100

    Add new function for grabbing prev and next designs.

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ function masore_filter_collection_query($query) {
   // Modify query for displaying designs in a collection, sorted by custom field.
   if(is_tax('collection') && !is_admin() && is_main_query()) {
     $query->set('posts_per_page', '50');
-    $query->set('orderby', 'meta_value');
+    $query->set('orderby', 'meta_value_num');
     $query->set('meta_key', 'number');
     $query->set('order', 'ASC');
   }
@@ -37,11 +37,35 @@ function masore_theme_enqueue_assets() {
 
 function masore_adjacent_post_link($output, $format, $link, $post, $adjacent) {
   if (empty($output)) {
-    $adjacent = $adjacent == 'next' ? 'previous' : 'next';
     return '<span class="inactive">' . str_replace('%link', $adjacent, $format) . '</span>';
   } else {
     return $output;
   }
+}
+
+function masore_adjacent_designs() {
+  $current = get_the_ID();
+  $return = (object)[];
+  $args = array(
+    'post_type' => 'design',
+    'orderby' => 'meta_value_num',
+    'meta_key' => 'number',
+    'order' => 'ASC',
+    'cache_results' => true
+  );
+  $query = new WP_Query($args);
+  foreach ($query->posts as $key => $post) {
+    if ($post->ID === $current) {
+      if ($query->posts[$key - 1]) {
+        $return->prev = new TimberPost($query->posts[$key - 1]->ID);
+      }
+      if ($query->posts[$key + 1]) {
+        $return->next = new TimberPost($query->posts[$key + 1]->ID);
+      }
+      break;
+    }
+  }
+  return $return;
 }
 
 add_action('pre_get_posts', 'masore_filter_collection_query');
