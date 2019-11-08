4328a3e60861eb48156a353a3322cdeb9de562ea
Jeppe <cenobitedk@users.noreply.github.com>
2019-11-08 11:46:13 +0100

    Update ss20.php

--- ss20.php
+++ ss20.php
@@ -4,4 +4,6 @@
  * Description: Masore frontpage.
  */
 $context = Timber::get_context();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
 Timber::render('page-ss20.twig', $context );
